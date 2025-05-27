4fb8fb4a337335186e6a305ab3bc0c511102400e
OptimusJc <optimus.prime.jc@gmail.com>
2025-05-27 18:23:09 +0300

    update configs

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,22 +1,21 @@
+
 return {
-	{
-		"stevearc/conform.nvim",
-		opts = {},
-	},
-	require("conform").setup({
-		formatters_by_ft = {
-			lua = { "stylua" },
-			-- Conform will run multiple formatters sequentially
-			python = { "isort", "autopep8" },
-			-- Use a sub-list to run only the first available formatter
-			javascript = { "prettierd", "prettier" },
-			typescript = { "prettierd", "prettier" },
-			javascriptreact = { "prettierd", "prettier" },
-			typescriptreact = { "prettierd", "prettier" },
-		},
-		format_on_save = {
-			timeout_ms = 500,
-			lsp_format = "fallback",
-		},
-	}),
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "autopep8" },
+        javascript = { "prettierd", "prettier" },
+        typescript = { "prettierd", "prettier" },
+        javascriptreact = { "prettierd", "prettier" },
+        typescriptreact = { "prettierd", "prettier" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    },
+  },
 }
+
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,81 +1,83 @@
 return {
-	{
-		"williamboman/mason.nvim",
+  {
+    "williamboman/mason.nvim",
 
-		config = function()
-			require("mason").setup({
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
 
-				ensure_installed = {
-					"lua_ls", -- Lua language server
-					"mdx_analyzer", -- MDX analyzer
-					"pyright", -- Python language server
-					"ts_ls", -- Typscript language server
-					"tailwindcss", -- Tailwind CSS language server
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			print("Loading LSP configurations")
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.mdx_analyzer.setup({})
-			lspconfig.pyright.setup({})
-			lspconfig.ts_ls.setup({})
-			lspconfig.tailwindcss.setup({
-				root_dir = function(fname)
-					return lspconfig.util.root_pattern(
-						"tailwind.config.ts",
-						"package.json",
-						".git"
-					)(fname) or vim.fn.getcwd()
-				end,
-				filetypes = {
-					"django-html",
-					"htmldjango",
-					"html",
-					"javascript",
-					"javascriptreact",
-					"typescript",
-					"typescriptreact",
-				},
-				cmd = { "tailwindcss-language-server", "--stdio" },
-			})
+        ensure_installed = {
+          "lua_ls",       -- Lua language server
+          "mdx_analyzer", -- MDX analyzer
+          "pyright",      -- Python language server
+          "tsserver",     -- Typscript language server
+          "tailwindcss",  -- Tailwind CSS language server
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      print("Loading LSP configurations")
+      local lspconfig = require("lspconfig")
+      local util = require("lspconfig.util")
 
-			vim.keymap.set(
-				"n",
-				"K",
-				vim.lsp.buf.hover,
-				{ desc = "Show hover information" }
-			)
-			vim.keymap.set(
-				"n",
-				"gd",
-				vim.lsp.buf.definition,
-				{ desc = "Go to definition" }
-			)
-			vim.keymap.set(
-				{ "n", "v" },
-				"<leader>ca",
-				vim.lsp.buf.code_action,
-				{ desc = "Code Action" }
-			)
-		end,
-	},
+      lspconfig.lua_ls.setup({})
+      lspconfig.mdx_analyzer.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.tsserver.setup({})
+      lspconfig.tailwindcss.setup({
+        root_dir = function(fname)
+          return util.root_pattern(
+            "tailwind.config.ts",
+            "package.json",
+            ".git"
+          )(fname) or vim.fn.getcwd()
+        end,
+        filetypes = {
+          "django-html",
+          "htmldjango",
+          "html",
+          "javascript",
+          "javascriptreact",
+          "typescript",
+          "typescriptreact",
+        },
+        cmd = { "tailwindcss-language-server", "--stdio" },
+      })
+
+      vim.keymap.set(
+        "n",
+        "K",
+        vim.lsp.buf.hover,
+        { desc = "Show hover information" }
+      )
+      vim.keymap.set(
+        "n",
+        "gd",
+        vim.lsp.buf.definition,
+        { desc = "Go to definition" }
+      )
+      vim.keymap.set(
+        { "n", "v" },
+        "<leader>ca",
+        vim.lsp.buf.code_action,
+        { desc = "Code Action" }
+      )
+    end,
+  },
 }
