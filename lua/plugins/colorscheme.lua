35904c0dc5f95d440aecd797b227b66f530b7ede
OptimusJc <optimus.prime.jc@gmail.com>
2025-08-26 14:20:39 +0300

    update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,33 +1,12 @@
 return {
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-
+		"rose-pine/neovim",
+		name = "rose-pine",
 		opts = {
-			flavour = "mocha",
-			transparent_background = true,
-			show_end_of_buffer = true,
-			integrations = {
-				treesitter = true,
-				cmp = true,
-				gitsigns = true,
-				telescope = true,
-				notify = true,
-				mini = true,
-				bufferline = true,
-				native_lsp = { enabled = true },
+			styles = {
+				transparency = true,
 			},
 		},
-		config = function(_, opts)
-			require("catppuccin").setup(opts)
-		end,
 	},
-
-	{
-		"lazyVim/lazyVim",
-		opts = {
-			colorscheme = "catppuccin",
-		},
-	},
+	{ "LazyVim/LazyVim", opts = { colorscheme = "rose-pine" } },
 }
